d8b58ac64b61c0aa7cd48e0d61e03c608ab1986e
LeonRabl11 <leon-rabl@web.de>
2025-10-16 11:36:00 +0200

    added start structur

--- Frontend/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import Navbar from "./UI/Navbar.tsx";
-import PostSkeleton from "./UI/PostSkeleton.tsx";
-import PostsSkeleton from "./UI/PostsSkeleton.tsx";
-import ProtectedRoute from "./UI/ProtectedRouter.jsx";
-
-export { Navbar, PostSkeleton, PostsSkeleton, ProtectedRoute };
---
+++ Frontend/src/pages/DetailContinent.tsx
@@ -0,0 +1,9 @@
+const DetailContinent = () => {
+  return (
+    <div>
+      <h1>Detail Continent Page</h1>
+    </div>
+  );
+};
+
+export default DetailContinent;
