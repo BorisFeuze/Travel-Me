86a2615b730e167aaed6be73b21e5c2bd43e41a7
Mateusz Olszewski <zeeevq1@gmail.com>
2025-10-27 17:21:41 +0100

    blabla

--- Frontend/src/components/UI/JobFilterCard.tsx
+++ Frontend/src/components/UI/JobFilterCard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { getAllJobOffers } from "@/data";
 import Filters from "./Filters";
 import { MessageSquare } from "lucide-react";
